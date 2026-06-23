d299acd3421421c47ed0997384c6bde991d4b179
Koushikkmrn <koushikkmrn@gmail.com>
2026-06-23 22:16:22 +0530

    prgm1

---
+++ Day 1/Discount.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.*;
+public class Discount{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        int p= sc.nextInt();
+        double bill;
+        if(p>=5000){
+            bill = p-(p*20.00/100);
+        }
+        else if(p>2000){
+            bill =p-(p*10.00/100);
+
+        }
+        else {
+            bill=p;
+        }
+        System.out.println("amount payable: $ "+bill);
+
+        
+                }     }
