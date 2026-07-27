f0b9c301b828bff501860a1fc77ef6d05a47adfd
Koushikkmrn <koushikkmrn@gmail.com>
2026-07-28 01:04:14 +0530

    '

---
+++ Day4/ConvertASCIIvaluetocharacter.java
@@ -0,0 +1,8 @@
+public class ConvertASCIIvaluetocharacter {
+    public static void main(String[] args) {
+        int ascii = 97;
+        char ch = (char) ascii;
+
+        System.out.println(ch);
+    }
+}
